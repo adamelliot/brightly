9089157dfeb768bf8fbe52d43cc58fe028110bfc
Adam Elliot <adam@warptube.com>
2009-11-23 09:22:00 -0700

    Added a passenger generator -p [path] to brightly command line

--- lib/brightly/provider/exec.rb
+++ lib/brightly/provider/exec.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'optparse'
+require 'fileutils'
 
 module Brightly
   module Provider
@@ -16,13 +17,29 @@ module Brightly
           opts.on('-p port')    { |val| options[:port] = val.to_i }
           
           opts.on('-r', '--rack-file', 'Prints the path to the builtin rack file and the contents of the file.') do
-            a = File.dirname(__FILE__).split(File::SEPARATOR)
-            a.slice!(-5, 5)
-            a += ['lib', 'brightly', 'provider']
-            config = File.join(a.join(File::SEPARATOR), 'config.ru')
+            config = rack_file
+
             puts "#{config}:"
             puts File.read(config)
             exit
+          end
+          
+          opts.on('-p path', '--passenger path', 'Generate an apache passenger config and directory structure.') do |val|
+            config = rack_file
+            
+            FileUtils.mkdir_p(File.join(val))
+            FileUtils.mkdir_p(File.join(val, 'tmp'))
+            FileUtils.mkdir_p(File.join(val, 'public'))
+
+            FileUtils.cp(config, File.join(val))
+
+            puts "You apache conf should look something like:"
+            puts <<-CONF
+<VirtualHost *:80>
+    ServerName brightly.local
+    DocumentRoot #{File.expand_path(File.join(val, 'public'))}
+</VirtualHost>
+CONF
           end
 
           opts.on_tail('-h', '--help', "Show this message") { puts opts ; exit }
@@ -30,6 +47,14 @@ module Brightly
 
         Brightly::Provider::Base.run! options
       end
+      
+      private
+        def rack_file
+          a = File.dirname(__FILE__).split(File::SEPARATOR)
+          a.slice!(-5, 5)
+          a += ['lib', 'brightly', 'provider']
+          config = File.join(a.join(File::SEPARATOR), 'config.ru')
+        end
     end
   end
 end
